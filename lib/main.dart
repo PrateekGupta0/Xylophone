4f13374538aeee25d5467d0675ddb1b35c322c2a
PrateekGupta0 <20UCS144@lnmiit.ac.in>
2022-04-13 11:38:42 +0530

    Xylophone_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
+// => arrow syntax
+
 class XylophoneApp extends StatelessWidget {
+  void sound(int index){
+    final player =AudioCache();
+    player.play('note$index.wav');
+  }
+  Expanded fun(int index,{Color color}/*named parameters*/){
+    return Expanded(
+      child: FlatButton(
+        onPressed: (){
+          sound(index);
+        },
+        child: Text(""),
+        color: color,
+      ),
+
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+
+              fun(1,color: Colors.red),
+              fun(2,color: Colors.orange),
+              fun(3,color: Colors.yellow),
+              fun(4,color: Colors.green),
+              fun(5,color: Colors.teal),
+              fun(6,color: Colors.blue),
+              fun(7,color: Colors.purple),
+
+              
+            ],
+          ),
         ),
       ),
     );
